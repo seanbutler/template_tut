c9ada022df45ba3dd901809f549c3fd57fc7a6ec
Sean Butler <butlersean@gmail.com>
2021-06-24 14:52:19 +0100

    step 3 - functions stored in type and also passed

--- main.cpp
+++ main.cpp
@@ -3,17 +3,31 @@
 
 // ----------------------------------------------------------------------
 
+struct IntOps {
+    static int clear () { return 0; };
+    static int add ( int x , int y ) { return x + y ; }
+};
+
+class StrOps {
+    public:
+        static std::string clear () { return ""; };
+        static std::string add ( std::string x , std::string y ) { return x +" "+ y ; }
+};
+
+// ----------------------------------------------------------------------
+
 template <typename T>
 struct MyList{
-    int head;
+    T head;
     struct MyList<T> *tail;
 };
 
-int LinkedInt_Total ( struct MyList<int> * ptr )
+template < typename T , typename O >
+T fold ( MyList<T> *ptr )
 {
-    int total = 0;
+    T total = O::clear();
     while ( ptr ) {
-        total += ptr->head;
+        total = O::add(total, ptr->head);
         ptr = ptr->tail;
     }
     return total;
@@ -21,20 +35,54 @@ int LinkedInt_Total ( struct MyList<int> * ptr )
 
 // ----------------------------------------------------------------------
 
+std::string ds[] = {
+                std::string("One"), 
+                std::string("Two"),
+                std::string("Three"), 
+                std::string("Four")
+            };
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    std::cout << "!!!\n";
 
-    MyList<int> *numListPtr;
-    MyList<int> *tmplistPtr;
+    {
+        MyList<int> *numListPtr;
+        MyList<int> *tmplistPtr;
 
-    tmplistPtr = (MyList<int>*)malloc(sizeof(MyList<int>));
-    numListPtr = tmplistPtr;
+        auto Total = fold<int, IntOps>;
 
-    for(int n=1;n<=10;n++){
-        tmplistPtr->head = n;
-        tmplistPtr->tail = (MyList<int>*)malloc(sizeof(MyList<int>));
-        tmplistPtr=tmplistPtr->tail;
-    }    
+        tmplistPtr = (MyList<int>*)malloc(sizeof(MyList<int>));
+        numListPtr = tmplistPtr;
 
-    std::cout << LinkedInt_Total(numListPtr);
+        for(int n=1;n<=10;n++){
+            tmplistPtr->head = n;
+            tmplistPtr->tail = (MyList<int>*)malloc(sizeof(MyList<int>));
+            tmplistPtr=tmplistPtr->tail;
+        }    
+        std::cout << Total(numListPtr) << std::endl;
+
+    }
+
+
+
+    {
+        MyList<std::string> *numListPtr;
+        MyList<std::string> *tmplistPtr;
+
+        auto Total = fold<std::string, StrOps>;
+
+        tmplistPtr = (MyList<std::string>*)malloc(sizeof(MyList<std::string>));
+        numListPtr = tmplistPtr;
+
+
+        for(int n=0;n<=3;n++){
+            tmplistPtr->head = ds[n];
+            tmplistPtr->tail = (MyList<std::string>*)malloc(sizeof(MyList<std::string>));
+            tmplistPtr=tmplistPtr->tail;
+        }    
+
+        std::cout << Total(numListPtr) << std::endl;
+    }
+
+
 }
