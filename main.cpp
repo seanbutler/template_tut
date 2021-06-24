53a6c9a3a0aed49fb1891c8d602cae52da614bfc
Sean Butler <butlersean@gmail.com>
2021-06-24 13:56:05 +0100

    step 2 - basic generic list

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 
 // ----------------------------------------------------------------------
 
-struct LinkedInt{
+template <typename T>
+struct MyList{
     int head;
-    struct LinkedInt *tail;
+    struct MyList<T> *tail;
 };
 
-int LinkedInt_Total ( struct LinkedInt * ptr )
+int LinkedInt_Total ( struct MyList<int> * ptr )
 {
     int total = 0;
     while ( ptr ) {
@@ -23,15 +24,15 @@ int LinkedInt_Total ( struct LinkedInt * ptr )
 int main(int, char**) {
     std::cout << "Hello, world!\n";
 
-    LinkedInt *numListPtr;
-    LinkedInt *tmplistPtr;
+    MyList<int> *numListPtr;
+    MyList<int> *tmplistPtr;
 
-    tmplistPtr = (LinkedInt*)malloc(sizeof(LinkedInt));
+    tmplistPtr = (MyList<int>*)malloc(sizeof(MyList<int>));
     numListPtr = tmplistPtr;
 
     for(int n=1;n<=10;n++){
         tmplistPtr->head = n;
-        tmplistPtr->tail = (LinkedInt*)malloc(sizeof(LinkedInt));
+        tmplistPtr->tail = (MyList<int>*)malloc(sizeof(MyList<int>));
         tmplistPtr=tmplistPtr->tail;
     }    
 
